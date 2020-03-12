47662abb2b1f430e78d8e9759cc25e6de9de8425
Ulyan Yunakh <ulyanyunakh@gmail.com>
2020-03-12 19:50:23 +0300

    картинки телефонов на мобилки

--- assets/js/main.js
+++ assets/js/main.js
@@ -107,15 +107,3 @@ $(window).on('load', function () {
 		'margin': 2 //Отступ между изображениями
 	});
 });
-
-var $btn = document.querySelector('.btn');
-
-$btn.addEventListener('click', e => {
-  window.requestAnimationFrame(() => {
-    $btn.classList.remove('is-animating');
-    
-    window.requestAnimationFrame(() => {
-      $btn.classList.add('is-animating');
-    });
-  });
-});
